7ef822679e00f6da64859dfe76fd231b02c39936
Jan Eglinger <jan.eglinger@fmi.ch>
2017-02-06 21:16:15 +0100

    Import fmi-ij2-plugins project for update site

---
+++ src/ch/fmi/knime/MaskStraightener.java
@@ -0,0 +1,72 @@
+package ch.fmi.knime;
+
+import java.awt.Polygon;
+import java.util.ArrayList;
+
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import ij.ImagePlus;
+import ij.gui.PolygonRoi;
+import ij.gui.Roi;
+import ij.plugin.Straightener;
+import ij.process.ByteProcessor;
+import ij.process.ImageProcessor;
+import sc.fiji.analyzeSkeleton.AnalyzeSkeleton_;
+import sc.fiji.analyzeSkeleton.Point;
+
+@Plugin(type = Command.class, headless = true, menuPath = "FMI>Straighten Image")
+public class MaskStraightener implements Command {
+        @Parameter(label = "Input image")
+        private ImagePlus imp;
+
+        @Parameter(label = "Object mask")
+        private ImagePlus mask;
+
+        @Parameter(label = "Line Width")
+        private int lineWidth;
+
+        @Parameter(label = "Step size")
+        private int stepSize;
+
+        @Parameter(type = ItemIO.OUTPUT)
+        private ImagePlus result;
+
+        @Override
+        public void run() {
+                ByteProcessor maskIp = mask.getProcessor().convertToByteProcessor(true);
+                maskIp.invert(); // work around IJ1 processors working with white background
+                maskIp.skeletonize();
+                maskIp.invert(); // work around IJ1 processors working with white background
+
+                // Initialize AnalyzeSkeleton_
+                AnalyzeSkeleton_ skel = new AnalyzeSkeleton_();
+                AnalyzeSkeleton_.calculateShortestPath = true;
+                skel.setup("", new ImagePlus("Skeleton", maskIp));
+
+                // Perform analysis in silent mode
+                skel.run(AnalyzeSkeleton_.NONE, false, true, null, true, false);
+
+                // Read the results
+                ArrayList<Point>[] sppoints = skel.getShortestPathPoints();
+
+                // Create polyline ROI by averaging pairs of points
+                Polygon poly = new Polygon();
+                for (int i = 0; i <= sppoints[0].size() - stepSize; i += stepSize) {
+                        int x = sppoints[0].get(i).x;
+                        int y = sppoints[0].get(i).y;
+                        poly.addPoint(x, y);
+                }
+                poly.addPoint(sppoints[0].get(sppoints[0].size() - 1).x, sppoints[0].get(sppoints[0].size() - 1).y);
+                PolygonRoi pRoi = new PolygonRoi(poly, Roi.POLYLINE);
+
+                // Create straightened image for kymograph etc.
+                imp.setRoi(pRoi);
+                Straightener st = new Straightener();
+                ImageProcessor ip = st.straightenLine(imp, lineWidth);
+
+                result = new ImagePlus("Straightened", ip);
+        }
+}
---
+++ src/ch/fmi/knime/TrackMateSpotDetector.java
@@ -0,0 +1,133 @@
+package ch.fmi.knime;
+
+import com.google.common.primitives.Doubles;
+import com.google.common.primitives.Ints;
+
+import java.util.ArrayList;
+
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.Spot;
+import fiji.plugin.trackmate.SpotCollection;
+import fiji.plugin.trackmate.TrackMate;
+import fiji.plugin.trackmate.detection.DetectorKeys;
+import fiji.plugin.trackmate.detection.LogDetectorFactory;
+import fiji.plugin.trackmate.tracking.LAPUtils;
+import fiji.plugin.trackmate.tracking.TrackerKeys;
+import fiji.plugin.trackmate.tracking.sparselap.SparseLAPTrackerFactory;
+import ij.ImagePlus;
+
+@Plugin(type = Command.class, headless = true, menuPath = "FMI>Spot Detection (Subpixel localization)")
+public class TrackMateSpotDetector implements Command {
+        @Parameter
+        private LogService log;
+
+        @Parameter(label = "Input image")
+        private ImagePlus imp;
+
+        @Parameter(label = "Spot radius")
+        private double spotSize = DetectorKeys.DEFAULT_RADIUS;
+
+        @Parameter(label = "Spot quality threshold")
+        private double spotThreshold = DetectorKeys.DEFAULT_THRESHOLD;
+
+        @Parameter(label = "Remove calibration?")
+        private boolean removeCalibration;
+
+        @Parameter(type = ItemIO.OUTPUT)
+        private int nSpotsFound;
+
+        @Parameter(type = ItemIO.OUTPUT)
+        private int[] spotID;
+
+        @Parameter(type = ItemIO.OUTPUT)
+        private double[] x;
+
+        @Parameter(type = ItemIO.OUTPUT)
+        private double[] y;
+
+        @Parameter(type = ItemIO.OUTPUT)
+        private double[] z;
+
+        @Parameter(type = ItemIO.OUTPUT)
+        private double[] quality;
+
+        // TODO optionally output intensity and radius (x,y,z?) for each spot
+
+        @Override
+        public void run() {
+                // Create TrackMate instance with settings
+                Model model = new Model();
+
+                // optionally remove calibration from imp
+                if (removeCalibration) {
+                        imp.setCalibration(null);
+                }
+
+                Settings settings = new Settings();
+
+                settings.setFrom(imp);
+
+                // TODO make detector choice optional
+                settings.detectorFactory = new LogDetectorFactory<>();
+
+                settings.detectorSettings = settings.detectorFactory.getDefaultSettings();
+                settings.detectorSettings.put(DetectorKeys.KEY_DO_SUBPIXEL_LOCALIZATION, true);
+                settings.detectorSettings.put(DetectorKeys.KEY_RADIUS, spotSize);
+                settings.detectorSettings.put(DetectorKeys.KEY_THRESHOLD, spotThreshold);
+
+                settings.trackerFactory = new SparseLAPTrackerFactory();
+                settings.trackerSettings = LAPUtils.getDefaultLAPSettingsMap();
+                settings.trackerSettings.put(TrackerKeys.KEY_LINKING_MAX_DISTANCE, TrackerKeys.DEFAULT_LINKING_MAX_DISTANCE);
+                settings.trackerSettings.put(TrackerKeys.KEY_GAP_CLOSING_MAX_DISTANCE, TrackerKeys.DEFAULT_GAP_CLOSING_MAX_DISTANCE);
+                settings.trackerSettings.put(TrackerKeys.KEY_GAP_CLOSING_MAX_FRAME_GAP, TrackerKeys.DEFAULT_GAP_CLOSING_MAX_FRAME_GAP);
+
+                // settings.detectorFactory.
+
+                TrackMate trackmate = new TrackMate(model, settings);
+
+                // Process (spot detection and tracking)
+                if (!trackmate.checkInput()) {
+                        log.error("Configuration error: " + trackmate.getErrorMessage());
+                        return;
+                }
+                if (!trackmate.process()) {
+                        log.error("Processing error: " + trackmate.getErrorMessage());
+                        return;
+                }
+
+                // Prepare lists to collect results
+                ArrayList<Integer> spotIDlist = new ArrayList<>();
+                ArrayList<Double> xList = new ArrayList<>();
+                ArrayList<Double> yList = new ArrayList<>();
+                ArrayList<Double> zList = new ArrayList<>();
+                ArrayList<Double> qList = new ArrayList<>();
+
+                // Get spot collection (all spots)
+                SpotCollection spotCollection = model.getSpots();
+                for (Spot spot : spotCollection.iterable(false)) {
+                        spotIDlist.add(spot.ID());
+                        xList.add(spot.getDoublePosition(0));
+                        yList.add(spot.getDoublePosition(1));
+                        zList.add(spot.getDoublePosition(2));
+                        qList.add(spot.getFeature(Spot.QUALITY));
+                }
+
+                // Get results (spot list with trackIDs)
+                spotID = Ints.toArray(spotIDlist);
+                x = Doubles.toArray(xList);
+                y = Doubles.toArray(yList);
+                z = Doubles.toArray(zList);
+                quality = Doubles.toArray(qList);
+
+                // Return summary values
+                nSpotsFound = model.getSpots().getNSpots(false);
+        }
+
+}
